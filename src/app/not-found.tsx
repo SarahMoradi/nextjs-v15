a47c43772f9d59fdab31a56dd7acd48b6fb36194
sarah <sarah@sarahs-MacBook-Air-2.local>
2025-02-09 12:23:38 +0330

    feat: add default pages for error | not-found | layout pages

---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default async function NotFound() {
+  return <div className='text-3xl flex justify-center items-center text-red-800 mx-auto'>Not Found :)</div>
+}
---
+++ src/app/student/courses/page.tsx
@@ -0,0 +1,7 @@
+export default async function Courses() {
+  return (
+    <div>
+      <h1>This is courses page</h1>
+    </div>
+  )
+}
