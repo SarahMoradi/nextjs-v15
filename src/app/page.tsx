42177666eea6b668c809107690bac494e1ffe8b3
sarah <sarah@sarahs-MacBook-Air-2.local>
2025-02-14 11:38:49 +0330

    feat: button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
-import {Colors} from './_components/colors/colors'
+import { Button } from './_components/button'
+// import {Colors} from './_components/colors/colors'
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         <div className='container flex flex-col-reverse items-center xl:flex-row'>
           <div className='flex-1 flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right'>
             <h3 className='text-xl xl:text-2xl dark:text-info'>خوش اومدی به ...</h3>
-            <h1 className=' text-3xl lg:text-5xl xl:text-5xl font-black gradient'>
+            <h1 className='text-3xl lg:text-5xl xl:text-5xl font-black gradient'>
               مسیر صعود به قله‌های برنامه‌نویسی
             </h1>
             <p className='max-w-2xl  text-lg md:text-xl font-bold leading-8'>
@@ -26,6 +27,11 @@ export default function Home() {
           <Image src='/images/programmer-landing.svg' alt='' width={702} height={521} />
         </div>
       </section>
+      <div className='container'>
+        <Button varient='info' size='large' shape='wide' animatedIcon={true}>
+          ثبت نام
+        </Button>
+      </div>
     </>
   )
 }
