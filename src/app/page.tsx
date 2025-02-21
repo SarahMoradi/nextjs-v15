ddaf5a443203aefbf2d53a5441ceed73cfc9897c
sarah <sarah@sarahs-MacBook-Air-2.local>
2025-02-21 22:26:31 +0330

    feat: fetch api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import { HomeHeroSection } from './_components/home-hero-section/home-hero-section'
+import {CourseSummary} from '@/types/course-summary.interface'
+import {HomeHeroSection} from './_components/home-hero-section/home-hero-section'
 // import {Colors} from './_components/colors/colors'
 
-export default function Home() {
+async function getNewestCourses(count: number): Promise<CourseSummary[]> {
+  const response = await fetch(`https://api.classbon.com/api/courses/newest/${count}`, {
+    next: {
+      revalidate: 24 * 60 * 60,  //based on seconds will be revalidated
+    },
+  })
+  return response.json()
+}
+
+export default async function Home() {
+  const newestCourses = await getNewestCourses(4)
+  console.log(newestCourses[0].title)
   return (
     <>
       <HomeHeroSection />
+      {newestCourses.map((post) => {
+        return <p key={post.title}>{post.title}</p>
+      })}
     </>
   )
 }
