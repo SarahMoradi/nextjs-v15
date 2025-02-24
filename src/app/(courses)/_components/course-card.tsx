81bbf0e8c66960782d69f2307be7adccd302fb9f
CaptainHook <moradisarah2001@gmail.com>
2025-02-24 17:19:41 +0330

    fix: card component"

--- src/app/(courses)/_components/course-card.tsx
+++ src/app/(courses)/_components/course-card.tsx
@@ -37,7 +37,7 @@ export const CourseCard: React.FC<CourseCardProps> = ({
           {title}
         </Link>
         <p>{subTitle}</p>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mt-3">
           <Badge variant="warning">
             <IconClock width={16} height={16} />
             {duration}
