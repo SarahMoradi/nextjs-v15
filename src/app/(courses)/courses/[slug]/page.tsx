4d97f139ab912935158ef5cef293861a5f3d18ae
CaptainHook <moradisarah2001@gmail.com>
2025-02-26 18:33:24 +0330

    feat: rating

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -2,19 +2,20 @@ import { API_URL } from "@/configs/global";
 import { CourseDetails } from "@/types/course-details.interface";
 import { notFound } from "next/navigation";
 
-
 //it should be defined in side of dynamic route segment for getting static rendring based on slugs
 
 //tip:
 //  The generateStaticParams function can be used in combination with dynamic route segments
 //  to statically generate routes at build time instead of on-demand at request time.
 //  The primary benefit of the generateStaticParams function is its smart retrieval of data.
-export async function generateStaticParams(){
-  const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) => res.json());
+export async function generateStaticParams() {
+  const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
+    res.json()
+  );
 
   return (slugs as string[]).map((slug: string) => ({
-    slug: slug
-  }))
+    slug: slug,
+  }));
 }
 
 async function getCourseDeatils(slug: string): Promise<CourseDetails> {
@@ -24,17 +25,30 @@ async function getCourseDeatils(slug: string): Promise<CourseDetails> {
 
 export default async function CourseDetail({
   params,
-}: { params: { slug: string }}) {
+}: {
+  params: { slug: string };
+}) {
   if (!params.slug) return notFound();
 
   const { slug } = params;
-  const courseData = await getCourseDeatils(slug);
+  const course = await getCourseDeatils(slug);
 
   return (
-    <div className="flex flex-col items-center justify-center mx-auto">
-      <h1 className="text-3xl font-bold text-center">Course Details</h1>
-      <p className="text-xl text-gray-600 mt-4">Slug: {slug}</p>
-      <span>{courseData.title}</span>
+    <div className="h-96 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
+      <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl"></div>
+      <div className="col-span-10 xl:col-span-7">
+        <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
+          {course.title}
+        </h1>
+        <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
+          {course.subTitle}
+        </h2>
+
+        <div className=" mt-5">Video Player Component</div>
+      </div>
+      <div className="col-span-10 xl:col-span-3 bg-secondary"></div>
+      <div className="col-span-10 xl:col-span-6 bg-info"></div>
+      <div className="col-span-10 xl:col-span-4 bg-warning"></div>
     </div>
   );
 }
---
+++ src/app/_components/rating/rating.tsx
@@ -0,0 +1,10 @@
+import { RatingProps } from "./rating.types";
+
+export const Rating: React.FC<RatingProps> = ({
+  rate,
+  className,
+  size = "normal",
+  variant = "warning",
+}) => {
+  return <div></div>;
+};
