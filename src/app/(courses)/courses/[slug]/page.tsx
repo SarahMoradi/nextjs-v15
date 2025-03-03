7ae9cfe15d4d2cfb8b2e18c57d14562dbee61a74
sarah <moradisarah2001@gmail.com>
2025-03-03 11:49:22 +0330

    feat: add tab

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -4,6 +4,8 @@ import {API_URL} from '@/configs/global'
 import {CourseDetails} from '@/types/course-details.interface'
 import {notFound} from 'next/navigation'
 import {CourseAside} from './_components/course-aside/course-aside'
+import {Tab} from '@/types/tab.type'
+import { Tabs } from '@/app/_components/tabs'
 
 //it should be defined in side of dynamic route segment for getting static rendring based on slugs
 
@@ -30,6 +32,21 @@ export default async function CourseDetail({params}: {params: {slug: string}}) {
   const {slug} = params
   const course = await getCourseDeatils(slug)
 
+  const tabs: Tab[] = [
+    {
+      label: 'مشخصات دوره',
+      content: course.description,
+    },
+    {
+      label: 'دیدگاه‌ها و پرسش',
+      content: 'course comments',
+    },
+    {
+      label: 'سوالات متداول',
+      content: 'accordion components',
+    },
+  ]
+
   return (
     <div className='container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10'>
       <div className='bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl'></div>
@@ -44,7 +61,9 @@ export default async function CourseDetail({params}: {params: {slug: string}}) {
       <div className='col-span-10 xl:col-span-3'>
         <CourseAside {...course} />
       </div>
-      <div className='col-span-10 xl:col-span-6 bg-info'></div>
+      <div className='col-span-10 xl:col-span-6'>
+        <Tabs tabs={tabs} />
+      </div>
       <div className='col-span-10 xl:col-span-4 bg-warning'></div>
     </div>
   )
