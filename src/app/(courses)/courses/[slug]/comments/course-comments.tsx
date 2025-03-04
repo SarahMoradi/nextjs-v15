0dcf8fa07e3fa559866a1b786a7db5bc4db91278
CaptainHook <moradisarah2001@gmail.com>
2025-03-04 17:30:19 +0330

    feat: course comments infinite scroll

--- src/app/(courses)/courses/[slug]/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/comments/course-comments.tsx
@@ -1,22 +1,76 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { useCourseComments } from "../_api/get-comments";
 import { Comment } from "@/app/_components/comment";
+import { TextPlaceholder } from "@/app/_components/placeholders";
+import { Fragment, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { Button } from "@/app/_components/button/button";
+import { IconRefresh } from "@/app/_components/icons/icons";
+import { useCourseComments } from "../_api/get-comments";
 
 const CourseComments = () => {
+  const { ref, inView } = useInView({});
   const { slug } = useParams();
-  const { data: comments } = useCourseComments({
+  const {
+    data: comments,
+    error,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  } = useCourseComments({
     params: {
       slug: slug as string,
       page: 1,
     },
   });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
+
+  if (error) {
+    return (
+      <>
+        <p>خطا در برقراری ارتباط با سرور</p>
+        <div className="text-center mt-3">
+          <Button
+            variant="neutral"
+            className="font-semibold"
+            isOutline={true}
+            shape="wide"
+            onClick={() => refetch()}
+          >
+            <IconRefresh />
+            تلاش مجدد
+          </Button>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
-      {comments?.data.map((comment) => (
-        <Comment key={`comment-${comment.id}`} {...comment} variant="info" />
+      {comments?.pages.map((currentPage) => (
+        <Fragment key={`comment-page-${currentPage}`}>
+          {currentPage.data.map((comment) => (
+            <Comment
+              key={`comment-${comment.id}`}
+              {...comment}
+              variant="info"
+            />
+          ))}
+        </Fragment>
       ))}
+
+      {(isFetching || hasNextPage) && (
+        <div ref={ref}>
+          <TextPlaceholder />
+        </div>
+      )}
     </>
   );
 };
