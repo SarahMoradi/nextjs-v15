51c9ce3d8a57541aa68e20e99ca5a216c9899e96
CaptainHook <moradisarah2001@gmail.com>
2025-03-04 16:33:15 +0330

    feat: comment component

--- src/app/(courses)/courses/[slug]/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/comments/course-comments.tsx
@@ -2,22 +2,23 @@
 
 import { useParams } from "next/navigation";
 import { useCourseComments } from "../_api/get-comments";
+import { Comment } from "@/app/_components/comment";
 
 const CourseComments = () => {
-    const { slug } = useParams();
-    const { data: comments } = useCourseComments({
-        params: {
-            slug: slug as string,
-            page: 1,
-        },
-    });
-    return (
-        <>
-            {comments?.data.map((p) => (
-                <p key={p.id} className="mb-8">{p.commentText}</p>
-            ))}
-        </>
-    );
+  const { slug } = useParams();
+  const { data: comments } = useCourseComments({
+    params: {
+      slug: slug as string,
+      page: 1,
+    },
+  });
+  return (
+    <>
+      {comments?.data.map((comment) => (
+        <Comment key={`comment-${comment.id}`} {...comment} variant="info" />
+      ))}
+    </>
+  );
 };
 
 export default CourseComments;
