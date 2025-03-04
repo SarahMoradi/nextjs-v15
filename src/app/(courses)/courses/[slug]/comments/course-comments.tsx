99b72bb4cafee21addee6f55d4678d8922bdc78e
CaptainHook <moradisarah2001@gmail.com>
2025-03-04 14:56:37 +0330

    feat: add api component for client-side

--- src/app/(courses)/courses/[slug]/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/comments/course-comments.tsx
@@ -1,3 +1,23 @@
 "use client";
 
+import { useParams } from "next/navigation";
+import { useCourseComments } from "../_api/get-comments";
 
+const CourseComments = () => {
+    const { slug } = useParams();
+    const { data: comments } = useCourseComments({
+        params: {
+            slug: slug as string,
+            page: 1,
+        },
+    });
+    return (
+        <>
+            {comments?.data.map((p) => (
+                <p key={p.id} className="mb-8">{p.commentText}</p>
+            ))}
+        </>
+    );
+};
+
+export default CourseComments;
