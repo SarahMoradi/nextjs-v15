8cea17e9aba16416abab6d4f03632cef1fc24200
CaptainHook <moradisarah2001@gmail.com>
2025-03-08 12:22:52 +0330

    feat: textbox component with react-hook-form

--- src/app/(auth)/sign-in/components/signin-form.tsx
+++ src/app/(auth)/sign-in/components/signin-form.tsx
@@ -2,14 +2,32 @@
 
 import { Button } from "@/app/_components/button/button";
 import { Textbox } from "@/app/_components/textbox";
+import { useForm } from "react-hook-form";
+import { SignIn } from "../types/signin.types";
 
 const SignInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignIn>();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <>
       <h5 className="text-2xl">ورود | ثبت نام</h5>
       <p className="mt-2">دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
-      <form className="flex flex-col gap-6 mt-16">
-        <Textbox placeholder="شماره موبایل" />
+      <form
+        className="flex flex-col gap-6 mt-16"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Textbox
+          placeholder="شماره موبایل"
+          {...register("mobile", { required: "شماره موبالی الزامی است" })}
+        />
         <Button type="submit" variant="primary">
           تایید و دریافت کد
         </Button>
