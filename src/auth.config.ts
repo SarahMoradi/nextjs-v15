d47fb372afa70dca65a985d77b4400e1ad51a987
CaptainHook <moradisarah2001@gmail.com>
2025-03-16 13:59:07 +0330

    feat: auth config

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,14 +1,30 @@
-import type {NextAuthConfig} from 'next-auth';
+import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
-    pages: {
-        signIn: '/sign-in',
-    },
-    callbacks: {
-        
-    },
-    providers: []
+  pages: {
+    signIn: "/sign-in",
+  },
+  callbacks: {
+    async authorized({ auth, request }) {
+      const { nextUrl } = request;
 
+      const isAuthenticated = !!auth?.user;
+
+      const authRoutes = ["/signin", "/verify"];
+      const isAuthRoutes = authRoutes.includes(nextUrl.pathname);
+
+      if (isAuthRoutes && isAuthenticated) {
+        return Response.redirect(new URL("/student/courses", nextUrl));
+      }
+
+      const isProtectedRoute = nextUrl.pathname.startsWith("/student");
+
+      if (isProtectedRoute && !isAuthenticated) {
+        return Response.redirect(new URL("/signin", nextUrl));
+      }
+
+      return true;
+    },
+  },
+  providers: [],
 } satisfies NextAuthConfig;
-
-
